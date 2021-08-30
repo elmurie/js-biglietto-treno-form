583588fbc152b386e051c379bead741028f1ac4a
elmurie <83522485+elmurie@users.noreply.github.com>
2021-08-30 14:04:03 +0200

    init repo

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// 1. seleziona i bottoni
+var btnGenera = document.getElementById('genera');
+var btnAnnulla = document.getElementById('annulla');
+
+// 2. al click del bottone genera 
+btnGenera.addEventListener('click', 
+    function() {
+        // logica di generazione del biglietto 
+
+        // 3. prendo i valori dei campi input e select
+        var nome = document.getElementById('nome').value;
+        var km = parseInt(document.getElementById('km').value);
+        var fasciaEta = document.getElementById('fascia').value;
+
+        // 4. calcolo il prezzo in base alla fascia d'età
+        var prezzo = km * 0.21;
+        if ( fasciaEta == "minorenne" ) {
+            // prezzo biglietto -20%
+            prezzo = prezzo - ( (prezzo / 100) * 20 );
+        } else if ( fasciaEta == "over") {
+            //prezzo biglietto -40%
+            prezzo = prezzo - ( (prezzo / 100) * 40 );
+        } 
+
+        // 5. stampo tutti i dati nel div del biglietto
+        // 5a. iniettare nome passeggero
+        document.getElementById('nome-passeggero').innerHTML = nome;
+        // 5b. iniettare tipo offerta
+        if ( fasciaEta == "minorenne" ) {
+            document.getElementById('tipo-offerta').innerHTML = "Sconto minorenni";
+        } else if ( fasciaEta == "over" ) {
+            document.getElementById('tipo-offerta').innerHTML = "Sconto over 65";
+
+        } else {
+            document.getElementById('tipo-offerta').innerHTML = "Biglietto Standard"
+        }
+        // 5c. iniettare prezzo
+        document.getElementById('totale').innerHTML = prezzo.toFixed(2) + " €";
+        // 5d. iniettare carrozza e codice CP
+        document.getElementById('carrozza').innerHTML = Math.floor( Math.random() * 10) + 1;
+        document.getElementById('codice-cp').innerHTML = Math.floor( Math.random() * ( 99999 - 90000 ) ) + 90000;
+
+
+        // rendi div visibile
+        document.getElementById("biglietto").classList.add("open");
+    }
+);
+
+// al click del bottone genera 
+btnAnnulla.addEventListener("click",
+    function() {
+        // nascondo il biglietto
+        document.getElementById("biglietto").classList.remove("open");
+
+        //reset tutti i campi input
+        document.getElementById('nome').value = "";
+        document.getElementById('km').value = "";
+        document.getElementById('fascia').value = "";
+
+    }
+);
